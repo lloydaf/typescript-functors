bc7929630a416c1d7e5f619c64876676e3e8c9c5
Lloyd Francis <lloydaf@gmail.com>
2018-10-04 23:44:41 +0530

    Adding a few more test cases

--- src/tests/functor.test.ts
+++ src/tests/functor.test.ts
@@ -11,5 +11,54 @@ test("Map an Array", () => {
 
 test("Map en empty array", () => {
   let arr = [];
-  expect(Functor.of(arr).map(x => x + 5).get()).toEqual([]);
+  expect(
+    Functor.of(arr)
+      .map(x => x + 5)
+      .get()
+  ).toEqual([]);
+});
+
+test("Map a string", () => {
+  let str: String = "Hello";
+  expect(
+    Functor.of(str)
+      .map(str => str + " world!")
+      .get()
+  ).toEqual("Hello world!");
+});
+
+test("Map a number", () => {
+  let num: number = 5;
+  expect(
+    Functor.of(num)
+      .map(num => num + 5)
+      .get()
+  ).toEqual(10);
+});
+
+test("Map an array of objects", () => {
+  let lannisters = [
+    {
+      name: "Tywin",
+      status: "Deceased"
+    },
+    {
+      name: "Tyrion",
+      status: "Alive"
+    },
+    {
+      name: "Cersei",
+      status: "Alive"
+    },
+    {
+      name: "Jamie",
+      status: "Alive"
+    }
+  ];
+  expect(Functor.of(lannisters).map(lannister => lannister.name).get()).toEqual([
+    "Tywin",
+    "Tyrion",
+    "Cersei",
+    "Jamie"
+  ]);
 });
